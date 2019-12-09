19ec9fa6d7483a810ddf74375429c71875c2e22e
HzgIgthorn <tcarl@eitco.de>
2019-12-09 12:05:49 +0100

    Update für tomcat bash shell versionierung

--- projekte/ubuntu14/provision-tomcat.sh
+++ projekte/ubuntu14/provision-tomcat.sh
@@ -2,4 +2,19 @@
 
 apt-get update -y
 apt-get upgrade -y
-apt-get install -y vim git openjdk-7-jdk-headless
+apt-get install -y vim git openjdk-7-jdk-headless dos2unix
+
+# cp /vagrant/inputrc /etc/inputrc
+
+cd /usr/local
+wget http://apache.mirror.digionline.de/tomcat/tomcat-8/v8.5.49/bin/apache-tomcat-8.5.49.tar.gz
+
+tar -xzvf apache-tomcat-8.5.49.tar.gz
+
+ln -s apache-tomcat-8.5.49 tomcat
+
+dos2unix /vagrant/tomcat8
+cp /vagrant/tomcat8 /etc/init.d/tomcat8
+chmod 755 /etc/init.d/tomcat8
+update-rc.d tomcat8 defaults
+service tomcat8 start
